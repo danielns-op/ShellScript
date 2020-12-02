45d7c644d247d540a0545d6699103d13b2828647
Danielns <danielnoronha.sh@gmail.com>
2020-12-02 16:16:04 -0300

    Arquivo inicial

---
+++ JogoDaForca/jogoforca.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+##########################################################
+# jogoforca.sh
+#
+# Autor: Daniel Noronha
+# Email: danielnoronha.sh@gmail.com
+#
+# Jogo da Forca em Shell Script, muito divertido!
+#
+# ChangeLog:
+# v0.1 - Criação do arquivo jogoforca.sh
+#      - Criação das váriveis de cores
+##########################################################
